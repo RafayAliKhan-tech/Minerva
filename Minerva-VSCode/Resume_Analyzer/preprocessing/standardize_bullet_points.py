bb094f110822c78a55b0b0ae62c5cd15fbf722f1
Abdur Rafay Ali Khan <abdurrafay.data@gmail.com>
2026-09-07 01:05:22 +0500

    recent change in standardize bullet point

--- Minerva-VSCode/Resume_Analyzer/preprocessing/standardize_bullet_points.py
+++ Minerva-VSCode/Resume_Analyzer/preprocessing/standardize_bullet_points.py
@@ -2,6 +2,6 @@ import re
 
 def normalize_bullets_by_line(text):
     common_bullet_point_list = ["•", "◦","‣","◉","▪","▫","■","□",  "◼","◻","▸","▹","➤","➣","–","—","✦","❖","❧","·","∙" ]
-    regex_pattern = r"^[" + "".join(common_bullet_point_list) + "]\s*"
+    regex_pattern = r"^[" + "".join(common_bullet_point_list) + r"]\s*"
     result = re.sub(regex_pattern, "- ", text, flags=re.MULTILINE)
     return result
