c58b63d655e94c394b85951a90906b34b0f88220
Abdur Rafay Ali Khan <abdurrafay.data@gmail.com>
2026-09-07 20:44:41 +0500

    do changes in program.cs from line 583 to 586

--- Minerva_Backend_Solution/Minerva_Backend/Program.cs
+++ Minerva_Backend_Solution/Minerva_Backend/Program.cs
@@ -580,7 +580,10 @@ if (!app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors("AllowFrontend");
 
